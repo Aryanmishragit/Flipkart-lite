6dae698c5e64b37a6809ff31bf77211fc169b46c
Aryanmishragit <solutioninfinite4u@gmail.com>
2026-01-05 23:18:11 +0530

    day2: product card and add to cart working

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import products from "./data/products";
 import ProductCard from "./components/ProductCard";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(product) {
+    setCart([...cart, product]);
+  }
+
+  useEffect(() => {
+    console.log("Updated Cart:", cart);
+  }, [cart]);
+
   return (
     <div>
       <h1>Flipkart Lite</h1>
+      <p>Cart Items: {cart.length}</p>
 
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <ProductCard
+          key={product.id}
+          product={product}
+          onAddToCart={addToCart}
+        />
       ))}
     </div>
   );
 }
 
 export default App;
-
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,16 @@
-function ProductCard({ product }) {
+function ProductCard({ product, onAddToCart }) {
   return (
     <div style={{ border: "1px solid #ccc", padding: "10px", margin: "10px" }}>
-      <img src={product.image} alt={product.title} />
+      <img
+        src={product.image}
+        alt={product.title}
+        style={{ width: "150px", display: "block" }}
+      />
       <h3>{product.title}</h3>
       <p>₹ {product.price}</p>
+      <button onClick={() => onAddToCart(product)}>
+        Add to Cart
+      </button>
     </div>
   );
 }
